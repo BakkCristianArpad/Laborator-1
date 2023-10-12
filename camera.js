e016dc0e89d3ebf4632fc2f802af4672413d1cbd
BakkCristianArpad <147048023+BakkCristianArpad@users.noreply.github.com>
2023-10-12 13:10:09 +0300

    Add files via upload

--- camera.js
+++ camera.js
@@ -4,6 +4,7 @@ var video = document.getElementById("video");
 function on_cam_success(stream){
 	video.srcObject = stream;	
 }
+
 function on_cam_error(err)
 {	
 	alert("error."+err.message);	
@@ -14,8 +15,8 @@ navigator.mediaDevices.getUserMedia(constraints)
 .then(on_cam_success)
 .catch(on_cam_error);
 
-function capteaza()
 
+function capteaza()
 {	
 	var c = document.getElementById("canvas");	
 	c.width = video.width;	
